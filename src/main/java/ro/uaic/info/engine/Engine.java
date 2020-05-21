61482e5bdde09c4d5fa44ba431e6f3c85de100b7
G2Gmycatisonfire <49059319+G2Gmycatisonfire@users.noreply.github.com>
2020-05-21 23:09:12 +0300

    Engine Basics

--- src/main/java/ro/uaic/info/engine/Engine.java
+++ src/main/java/ro/uaic/info/engine/Engine.java
@@ -1,5 +1,7 @@
 package ro.uaic.info.engine;
 
+import ro.uaic.info.engine.exception.EngineExceptionUninitialized;
+
 public class Engine {
     private static Engine instance;
 
@@ -10,10 +12,18 @@ public class Engine {
      */
     private int debugLevel = 0;
 
+    private boolean isActive = false;
+
+    public static int DEFAULT_FPS_TARGET = 60;
+
     public static final int DEBUG_LEVEL_NONE = 0;
     public static final int DEBUG_LEVEL_CRITICAL = 1;
     public static final int DEBUG_LEVEL_IMPORTANT = 2;
     public static final int DEBUG_LEVEL_ALL_MESSAGES = 20;
+
+    private int targetFPS = DEFAULT_FPS_TARGET;
+
+    private boolean initialized = false;
 
     public Engine setDebugLevel(int debugLevel){
         this.debugLevel = debugLevel;
@@ -30,16 +40,51 @@ public class Engine {
 
     }
 
-    private void initialiseEngine(){
+    private Engine initialiseEngine(){
 
+        this.initialized = true;
+        return this;
     }
 
+    /**
+     * Will call ALL objects in engine that need updating every frame
+     * @return
+     */
     private void update(){
 
     }
 
-    public void run(){
+    public synchronized void run(boolean initAtRuntime) throws EngineExceptionUninitialized {
+        if(initAtRuntime)
+            this.initialiseEngine();
 
+        if(!initialized)
+            throw new EngineExceptionUninitialized();
+
+        this.isActive = true;
+
+        long startFrameTime = 0;
+        long endFrameTime = 0;
+
+        int updateTime;
+        int sleepTimer = (1000 / this.targetFPS); // 1000ms / frames per Second = one frame's duration
+
+        while(this.isActive){
+            startFrameTime = System.nanoTime();
+
+            this.update();
+
+            endFrameTime = System.nanoTime();
+
+            updateTime = (int) ((endFrameTime - startFrameTime) / 1000000);
+
+            try{
+                if(sleepTimer >= updateTime)
+                    Thread.sleep(sleepTimer - updateTime);
+            } catch (InterruptedException ignored){
+
+            }
+        }
     }
 
 }
