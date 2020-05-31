252efbc15a936b100769fd29edd050af77d82f1a
G2Gmycatisonfire <49059319+G2Gmycatisonfire@users.noreply.github.com>
2020-06-01 00:25:22 +0300

    Big Class

---
+++ src/main/java/ro/uaic/info/game/objects/GameObject.java
@@ -0,0 +1,9 @@
+package ro.uaic.info.game.objects;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GameObject {
+    private Rectangle mesh;
+    private BufferedImage sprite;
+}
